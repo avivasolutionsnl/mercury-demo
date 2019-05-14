3c902cefcecd8358e88d8d1c9d25de440683fecf
Joost Meijles <joost@meijles.com>
2019-05-14 13:39:19 +0200

    Add files

---
+++ REAMDE.md
@@ -0,0 +1,28 @@
+# Setup instructions
+These setup instructions will help you setup and run the Mercury Demo.
+
+## Prerequisites
+1. A valid sitecore license xml file
+2. Windows 10 64 bit build 17763 or higher (press windows key, type in `run`, hit enter, type in `winver`, hit enter to check)
+
+## Setting up the demo environment
+1. Put the `docker-compose.yml`, `license.xml` and `setup.ps1` in a folder of choice. These files are needed to start the environment so don't delete these.
+2. Open up a powershell with admin rights, navigate to this folder (`cd <insertpath to folder here>`) and run `./setup.ps1 -ExecutionPolicy UnRestricted`
+     - Your computer may be asked restart to enable hyper-v. Press yes.
+3. Switch docker to `Windows Containers` by clicking on `Switch to Windows containers` in the menu that appears when you right click on `Docker Dekstop` in the `hidden icons` panel that can be located near your clock.
+
+## Using the demo environment
+1. Open up a powershell terminal and navigate to the folder where you put the `docker-compose.yml` 
+     - Make sure you open up a new powershell after running `setup.ps1`. This will refresh the PATH variables.
+2. Run `az login` to generate a login token and then `az acr login --name avivasolutionsnl` to gain access to our docker images. 
+     - `az login` will open up a browser window where you can login. If you use the Edge browser it will hang at the login process. This step should work with the Chrome browser.
+3. Run `docker-compose up`. The first time this can take a while as it has to download the environment, once downloaded you can skip step 2 the next time.
+4. Now you can go to the following urls:
+     - http://www.mercurydemo.localhost to view the site, 
+     - http://www.mercurydemo.localhost/sitecore to view the cms 
+     - https://commerce.mercurydemo.localhost:4200 to view commerce
+5. You can stop the environment with `docker-compose down`
+
+## Updating the demo environment (for when new versions of the demo are released)
+1. Open a powershell and run `az acr login --name avivasolutionsnl`
+2. Navigate to the folder with the `docker-compose.yml` and run `docker-compose pull`. This can take a while.
---
+++ setup.ps1
@@ -0,0 +1,73 @@
+#Requires -RunAsAdministrator
+Param(
+    [string]
+    $currentFolder = "$PSScriptRoot",
+    [string]
+    $licenseFile = "$PSScriptRoot/license.xml",
+    [string]
+    $composeFile = "$PSScriptRoot/docker-compose.yml"
+)
+
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
+
+If (-Not (Test-Path($licenseFile))) {
+    throw "license.xml file at $licenseFile not found"
+}
+
+If (-Not (Test-Path("$composeFile"))) {
+    throw "docker-compose.yml file at $composeFile not found"
+}
+
+Write-Host "Setting the file permissions...."
+takeown.exe /R /F $currentFolder
+icacls.exe $currentFolder /t /c /GRANT 'Users:F'
+icacls.exe $currentFolder /t /c /GRANT 'Authenticated Users:F'
+Write-Host "Setting file permissions finished!" -ForegroundColor Green
+
+$dockerIsInstalled = ((Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall") |  Where-Object { $_.GetValue( "DisplayName" ) -like "*docker*" } ).Length;
+
+if ($dockerIsInstalled -eq 0) {
+    $dockerInstalexe = "$currentFolder/dockerInstal.exe"
+
+    If (-Not (Test-Path($dockerInstalexe))) {
+        $dockerInstalDownload = "$currentFolder/dockerInstal.exe"
+        Write-Host "Downloading docker...."
+        Invoke-WebRequest -Uri https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe -OutFile $dockerInstalDownload
+        Move-Item -Path $dockerInstalDownload -Destination $dockerInstalexe
+        Write-Host "Download of docker finished!" -ForegroundColor Green
+    }
+    else {
+        Write-Host "Docker already downloaded!" -ForegroundColor Green
+    }
+    Write-Host "Installing docker...."
+    start-process -wait $dockerInstalexe " install --quiet"
+    Write-Host "Docker installed!" -ForegroundColor Green
+
+    Write-Host "Starting docker...."
+    start-process "$env:ProgramFiles\docker\Docker\Docker for Windows.exe"
+    Write-Host "Docker started!" -ForegroundColor Green
+}else{
+    Write-Host "Docker already installed!" -ForegroundColor Green
+}
+
+$azureCliIsInstalled = (Get-Module -ListAvailable -Name Azure -Refresh).Count
+if ($azureCliIsInstalled -eq 0) {
+    $azureclimsi = "$currentFolder/azurecli.msi"
+    If (-Not (Test-Path($azureclimsi))) {
+        Write-Host "Downloading the azure cli...."
+        Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile $azureclimsi
+        Write-Host "Download of azure cli finished!" -ForegroundColor Green
+    }
+    else {
+        Write-Host "Azure cli already downloaded!" -ForegroundColor Green
+    }
+    Write-Host "Installing the azure cli...."
+    Start-Process msiexec.exe -Wait -ArgumentList '/I $azureclimsi /quiet'
+    RefreshEnv.cmd
+    Write-Host "Installation of azure cli finished!" -ForegroundColor Green
+}
+else {
+    Write-Host "Azure cli already installed!" -ForegroundColor Green
+}
+Write-Host "Setup finished!" -ForegroundColor Green
